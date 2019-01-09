75663fb82538ba948f2d8b88068bdfc86adbacf3
Viacheslav <demon55591@yandex.ru>
2019-01-09 12:33:10 +0300

    Commit after chapter 5 (fixed bugs and eliminated possibility of cheating)

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,13 +1,69 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String ANSWER_SHOWN_KEY = "answer shown";
+    private static final String EXTRA_ANSWER_SHOWN =
+            "com.bignerdranch.android.geoquiz.answer_shown";
+    private static final String TAG = "CheatActivity";
+
+    Button cheatButton;
+    TextView answerTextView;
+    Boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        answerTextView = findViewById(R.id.answer_text_view);
+        if (savedInstanceState != null) {
+            mIsCheater = savedInstanceState.getBoolean(ANSWER_SHOWN_KEY);
+            if (mIsCheater) {
+                answerTextView.setText((getIntent()
+                        .getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false))
+                        ? R.string.true_button : R.string.false_button);
+                setAnswerShownResult(mIsCheater);
+            }
+        }
+
+
+        (cheatButton = findViewById(R.id.show_answer_button)).setOnClickListener(v -> {
+            answerTextView.setText((getIntent()
+                    .getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false))
+                    ? R.string.true_button : R.string.false_button);
+            setAnswerShownResult(true);
+        });
+
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+        mIsCheater = isAnswerShown;
+    }
+
+    public static Intent newIntent(Context context, boolean answerIsTrue) {
+        return new Intent(context, CheatActivity.class).putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+    }
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putBoolean(ANSWER_SHOWN_KEY, mIsCheater);
     }
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +16,9 @@ import com.bignerdranch.android.geoquiz.model.Question;
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String INDEX_KEY = "index";
+    private static final String ANSWER_SHOWN_KEY = "answer shown";
+    private static final int REQUEST_CODE_CHEAT = 0;
+    private boolean mIsCheater;
 
 
     private TextView mQuestionTextView;
@@ -20,6 +26,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
@@ -38,8 +45,10 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
-        if (savedInstanceState != null)
+        if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(INDEX_KEY, 0);
+            mIsCheater = savedInstanceState.getBoolean(ANSWER_SHOWN_KEY, false);
+        }
 
         mQuestionTextView = findViewById(R.id.textView);
 
@@ -61,18 +70,41 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton = findViewById(R.id.next_button);
         mNextButton.setOnClickListener(v -> {
             mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            mIsCheater = false;
             updateQuestion();
         });
+
+        (mCheatButton = findViewById(R.id.cheat_button)).setOnClickListener(v ->
+                startActivityForResult(
+                        CheatActivity.newIntent(QuizActivity.this, mQuestionBank[mCurrentIndex].isAnswerTrue()),
+                        REQUEST_CODE_CHEAT));
+
 
         updateQuestion();
 
     }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) { // before onStop()
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState");
         outState.putInt(INDEX_KEY, mCurrentIndex);
+        outState.putBoolean(ANSWER_SHOWN_KEY, mIsCheater);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
 
     private void updateQuestion() {
@@ -83,11 +115,16 @@ public class QuizActivity extends AppCompatActivity {
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean rightAnswer = mQuestionBank[mCurrentIndex].isAnswerTrue();
-        if (userPressedTrue == rightAnswer) {
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        if (mIsCheater)
+            Toast.makeText(this, R.string.judgment_toast, Toast.LENGTH_SHORT).show();
+        else {
+            if (userPressedTrue == rightAnswer) {
+                Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
         }
+
     }
 
     @Override
